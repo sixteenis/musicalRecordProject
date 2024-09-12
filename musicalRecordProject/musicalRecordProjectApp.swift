1cf71e4f45ee3dd7c05a54220bd70055f9372c40
sixteenis <psm0103907@naver.com>
2024-09-12 15:54:44 +0900

    Initial Commit

---
+++ musicalRecordProject/musicalRecordProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicalRecordProjectApp.swift
+//  musicalRecordProject
+//
+//  Created by 박성민 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct musicalRecordProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
