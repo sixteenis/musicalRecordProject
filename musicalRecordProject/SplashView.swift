8bcf8ab20dbbaba367a0eab175a4480fde07e141
sixteenis <psm0103907@naver.com>
2024-09-30 23:50:26 +0900

    (출시) V1.0.0 - splashView 추가

---
+++ musicalRecordProject/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  musicalRecordProject
+//
+//  Created by 박성민 on 9/30/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
